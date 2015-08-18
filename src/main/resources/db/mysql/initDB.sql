970f77a91ef94ccf6eed1ccf4ef3fb4095d4b71f
saschagrebe <sascha.grebe@adesso.de>
2015-08-18 21:45:05 +0200

    switch from hsql to mysql and remove indices for bad performance reasons

--- src/main/resources/db/mysql/initDB.sql
+++ src/main/resources/db/mysql/initDB.sql
@@ -6,28 +6,23 @@ USE petclinic;
 CREATE TABLE IF NOT EXISTS vets (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  INDEX(last_name)
+  last_name VARCHAR(30)
 ) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS specialties (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
+  name VARCHAR(80)
 ) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS vet_specialties (
   vet_id INT(4) UNSIGNED NOT NULL,
   specialty_id INT(4) UNSIGNED NOT NULL,
-  FOREIGN KEY (vet_id) REFERENCES vets(id),
-  FOREIGN KEY (specialty_id) REFERENCES specialties(id),
-  UNIQUE (vet_id,specialty_id)
+  UNIQUE (vet_id, specialty_id)
 ) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS types (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
+  name VARCHAR(80)
 ) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS owners (
@@ -36,8 +31,7 @@ CREATE TABLE IF NOT EXISTS owners (
   last_name VARCHAR(30),
   address VARCHAR(255),
   city VARCHAR(80),
-  telephone VARCHAR(20),
-  INDEX(last_name)
+  telephone VARCHAR(20)
 ) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS pets (
@@ -45,16 +39,12 @@ CREATE TABLE IF NOT EXISTS pets (
   name VARCHAR(30),
   birth_date DATE,
   type_id INT(4) UNSIGNED NOT NULL,
-  owner_id INT(4) UNSIGNED NOT NULL,
-  INDEX(name),
-  FOREIGN KEY (owner_id) REFERENCES owners(id),
-  FOREIGN KEY (type_id) REFERENCES types(id)
+  owner_id INT(4) UNSIGNED NOT NULL
 ) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS visits (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   pet_id INT(4) UNSIGNED NOT NULL,
   visit_date DATE,
-  description VARCHAR(255),
-  FOREIGN KEY (pet_id) REFERENCES pets(id)
+  description VARCHAR(255)
 ) engine=InnoDB;
